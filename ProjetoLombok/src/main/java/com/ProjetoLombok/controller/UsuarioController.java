b41eb6e814036d3084849bc4af8044622cc70a2d
victorgeroto <132359789+victorgeroto@users.noreply.github.com>
2023-10-17 08:27:29 -0300

    Add files via upload

---
+++ ProjetoLombok/src/main/java/com/ProjetoLombok/controller/UsuarioController.java
@@ -0,0 +1,41 @@
+package com.ProjetoLombok.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ProjetoLombok.entities.Usuario;
+import com.ProjetoLombok.services.UsuarioService;
+
+@RestController
+@RequestMapping
+public class UsuarioController {
+	
+	private final UsuarioService usuarioService;
+	
+	@Autowired
+	public UsuarioController(UsuarioService usuarioService) {
+		this.usuarioService = usuarioService;
+	}
+	@GetMapping("/(id)")
+	public ResponseEntity <Usuario> buscaUsuarioControlId(@PathVariable Long id){
+		Usuario usuario = usuarioService.buscaUsuarioId(id);
+		if(usuario != null) {
+			return ResponseEntity.ok(usuario);
+		}
+		else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	@GetMapping("/")
+	public ResponseEntity<List<Usuario>> buscaTodosUsuariosControl(){
+		List<Usuario>Usuarios = usuarioService.buscaTodosUsuarios();
+		return ResponseEntity.ok(Usuarios);
+	}
+	
+}
